b2b9a833f6c2ffdae9038b871051e8c6bac27ed4
Pascal <pascal@pascal.com>
2023-09-19 21:26:06 -0400

    og tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,16 @@
 	import './page.less';
 </script>
 
+<svelte:head>
+	<title>can i has :has?</title>
+	<meta name="description" content="The question on every web developer’s mind." />
+	<meta property="og:title" content="can i has :has?" />
+	<meta property="og:description" content="The question on every web developer’s mind." />
+	<meta property="og:url" content="https://canihashas.com" />
+	<meta property="og:site_name" content="can i has :has?" />
+	<meta property="og:image" content="https://canihashas.com/canihashas.jpg" />
+</svelte:head>
+
 <main>
 	<figure>
 		<a href="https://caniuse.com/css-has">
